fa6659de121bfe69aba3b50d92088a9ccfba4b43
Chanesk <chanesk01@gmail.com>
2023-09-04 21:41:31 +0300

    feat: spinner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { LandingPageModule } from './landing-page/landing-page.module';
 import { LazyImgDirectiveDirective } from './lazy-img-directive.directive';
+import { SpinnerComponent } from './spinner/spinner/spinner.component';
+import { LoaderService } from './core/service/loader.service';
+import { LoaderInterceptor } from './interceptors/loader.interceptor';
 
 @NgModule({
   declarations: [
     AppComponent,
     LazyImgDirectiveDirective,
+    SpinnerComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     CoreModule,
     LandingPageModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: LoaderInterceptor,
+      multi:true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
